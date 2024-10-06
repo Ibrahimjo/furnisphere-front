c9b6d8f0ac9039c6cc57f297f1cf4405725ffda7
Ibrahim Aljork <ibrahimaljork98@gmail.com>
2024-10-06 14:41:14 +0300

    Updates

--- _next/static/chunks/app/about/page-4f579b14b0812aa4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,404,768,979,834,841,726,998],{63521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
--- _next/static/chunks/app/docs/layout-a9364e27e0fb082c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[998,404,768,979,834,301,841,726],{63521:function(){}},function(n){n.O(0,[971,23,744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
